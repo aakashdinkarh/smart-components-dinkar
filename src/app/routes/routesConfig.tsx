827cc93b1626a66b44889e04a5f45941011fdadd
Dinkar <aakashdinkarh@gmail.com>
2023-12-30 00:46:21 +0530

    import react fix

--- src/app/routes/routesConfig.tsx
+++ src/app/routes/routesConfig.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import { ButtonPage } from './Button'
 import { NpmPackagePage } from './NpmPackage';
--- src/components/Select/useCustomSelect.ts
+++ src/components/Select/useCustomSelect.ts
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 import { getVisibleOptions, setNewFocus } from './helpers';
 import type { IuseCustomSelect, Option,useCustomSelectArgs } from './interfaces';
