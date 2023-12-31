4e0f76788226eda564843b62e536c378f8ea6ea8
Dinkar <aakashdinkarh@gmail.com>
2023-12-31 23:17:06 +0530

    custom rule fix script

--- customStylelint/no-css-vars-outside-app.js
+++ customStylelint/no-css-vars-outside-app.js
@@ -1,14 +1,20 @@
+/* eslint-disable no-console */
 const path = require('path');
 
 const stylelint = require('stylelint');
 
 const ruleName = 'custom/no-css-vars-outside-app';
-
 const pathToSrcApp = path.join(__dirname, '..', 'src/app');
 
 const messages = stylelint.utils.ruleMessages(ruleName, {
-	rejected: 'Avoid the use of CSS variables outside the "src/app" directory.',
+	rejected : 'Avoid the use of CSS variables outside the "src/app" directory.',
+	fixed    : (originalVal, fixedVal) => `✅ Changed ${originalVal} --> ${fixedVal}`,
 });
+
+const { parseVariableFile } = require('../scripts/parseCssVarFile');
+
+const cssVariableFilePath = path.join(__dirname, '../', 'src/app/styles.module.css');
+const cssVariables = parseVariableFile(cssVariableFilePath);
 
 module.exports = stylelint.createPlugin(ruleName, (enabled) => {
 	return (root, result) => {
@@ -16,7 +22,6 @@ module.exports = stylelint.createPlugin(ruleName, (enabled) => {
 			return;
 		}
 
-		// Check the file path to determine if it's outside "src/app/"
 		const filePath = root.source.input.file;
 		if (!filePath || filePath.includes(pathToSrcApp)) {
 			return;
@@ -24,12 +29,21 @@ module.exports = stylelint.createPlugin(ruleName, (enabled) => {
 
 		root.walkDecls((decl) => {
 			if (decl.value.includes('var(--')) {
-				stylelint.utils.report({
-					ruleName,
-					result,
-					node    : decl,
-					message : messages.rejected,
+				const fixedValue = decl.value.replace(/var\(([^)]+)\)/g, (_, variableName) => {
+					return cssVariables[variableName];
 				});
+
+				if (fixedValue && result.stylelint.config.fix) {
+					console.log(messages.fixed(decl.value, fixedValue));
+					decl.value = fixedValue;
+				} else {
+					stylelint.utils.report({
+						ruleName,
+						result,
+						node    : decl,
+						message : messages.rejected,
+					});
+				}
 			}
 		});
 	};
@@ -37,3 +51,6 @@ module.exports = stylelint.createPlugin(ruleName, (enabled) => {
 
 module.exports.ruleName = ruleName;
 module.exports.messages = messages;
+module.exports.meta = {
+	fixable: true
+}
