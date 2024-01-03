0a15e1b9e34df563dd059e52d00602088ccc3444
Dinkar <aakashdinkarh@gmail.com>
2024-01-03 13:14:21 +0530

    move dependencies to package.json

--- scripts/build-app.js
+++ scripts/build-app.js
@@ -3,12 +3,10 @@ const fs = require('fs/promises');
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
-const additionalPackagesForWebsiteBuild = ["react", "react-dom", "react-router-dom"];
-
 async function handleBuildApp ({ newPackageJson, packageJson }) {
 	try {
 		await fs.writeFile('package.json', JSON.stringify(newPackageJson, null, 2));
-		console.log(`Added ${additionalPackagesForWebsiteBuild.join(', ')} to dependencies.`);
+		console.log(`Added ${packageJson['website-prod-dependencies'].join(', ')} to dependencies.`);
 		
 		const { stdout, stderr } = await exec('npx react-scripts build');
 
@@ -18,7 +16,7 @@ async function handleBuildApp ({ newPackageJson, packageJson }) {
 		console.error('Something went wrong', err)
 	} finally {
 		await fs.writeFile('package.json', JSON.stringify(packageJson, null, 2));
-		console.log(`Removed ${additionalPackagesForWebsiteBuild.join(', ')} from dependencies.`);
+		console.log(`Removed ${packageJson['website-prod-dependencies'].join(', ')} from dependencies.`);
 	}
 }
 
@@ -28,14 +26,18 @@ if (process.env.BUILD_TARGET === 'react-app') {
 		const fileContent = await fs.readFile('package.json', 'utf-8');
 		const packageJson = JSON.parse(fileContent);
 		const newPackageJson = JSON.parse(fileContent);
+
+		const websiteProdDependencies = packageJson['website-prod-dependencies'];
 	
 		newPackageJson.dependencies = {
 			...newPackageJson.dependencies || {},
 		};
 	
-		additionalPackagesForWebsiteBuild.forEach((packageName) => {
+		websiteProdDependencies.forEach((packageName) => {
 			newPackageJson.dependencies[packageName] = newPackageJson.devDependencies[packageName];
-		})
+		});
+
+		delete newPackageJson['website-prod-dependencies'];
 	
 		handleBuildApp({ newPackageJson, packageJson });
 	})();
