fe22f31de442c7022f91923ccd965f2525e3ab24
Dinkar <aakashdinkarh@gmail.com>
2024-01-06 03:24:24 +0530

    npm package tutorial description added, loader added

--- src/app/routes/NpmPackage/index.tsx
+++ src/app/routes/NpmPackage/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
-import { CodeWrapper, Select } from '../../../exports'
-import { getCombinedClass } from "../../../utils/getCombinedClass";
+import { CodeWrapper, Select, Loader } from '../../../exports';
+import { getCombinedClass } from '../../../utils/getCombinedClass';
 import { highlightCode } from '../../helpers/highlightCode';
 
 import styles from './styles.module.css';
@@ -17,74 +17,75 @@ interface tutorialStep {
 }
 
 const THEME_OPTIONS = [
-	{ label: "Github dark", value: "github-dark-min" },
-	{ label: "Darcula", value: "base16-darcula-min" },
-	{ label: "Sublime", value: "monokai-sublime-min" },
-	{ label: "Atom one dark", value: "atom-one-dark" },
-	{ label: "Hardcore", value: "base16-hardcore-min" },
-	{ label: "Material", value: "base16-material-min" },
+	{ label: 'Github dark', value: 'github-dark-min' },
+	{ label: 'Darcula', value: 'base16-darcula-min' },
+	{ label: 'Sublime', value: 'monokai-sublime-min' },
+	{ label: 'Atom one dark', value: 'atom-one-dark' },
+	{ label: 'Hardcore', value: 'base16-hardcore-min' },
+	{ label: 'Material', value: 'base16-material-min' },
 ];
 
-function InstructionWrapper({ title, subText, code, nestedSteps, isCodeHighlighted = false }: tutorialStep){
+function InstructionWrapper({ title, subText, code, nestedSteps, isCodeHighlighted = false }: tutorialStep) {
 	return (
-		<div className={getCombinedClass(styles['instruction-wrapper'])} >
+		<div className={getCombinedClass(styles['instruction-wrapper'])}>
 			<h3 dangerouslySetInnerHTML={{ __html: title }} />
 
-			{subText != null ? (
-				<p dangerouslySetInnerHTML={{ __html: subText }} />
-			) : null}
+			{subText != null ? <p dangerouslySetInnerHTML={{ __html: subText }} /> : null}
 
 			{nestedSteps ? (
-				nestedSteps.map((step) => (
-					<InstructionWrapper isCodeHighlighted={isCodeHighlighted} {...step} />
-				))
+				nestedSteps.map((step) => <InstructionWrapper isCodeHighlighted={isCodeHighlighted} {...step} />)
 			) : (
 				<CodeWrapper isCodeHighlighted={isCodeHighlighted}>{code}</CodeWrapper>
 			)}
 		</div>
-	)
+	);
 }
 
-function StickyWrapper({ children }){
+function StickyWrapper({ children }) {
 	return (
-		<div className='sticky border-b bg-white z1 width-full-section' style={{ top: 0 }} >
+		<div className='sticky border-b bg-white z1 width-full-section' style={{ top: 0 }}>
 			{children}
 		</div>
 	);
 }
 
-export function NpmPackagePage(){
-	const [isCodeHighlighted, setIsCodeHighlighted] = useState(false);
+export function NpmPackagePage() {
+	const [isCodeHighlighted, setIsCodeHighlighted] = useState<boolean | null>(null);
 	const [codeStyleTheme, setCodeStyleTheme] = useState(THEME_OPTIONS[0].value);
 
 	useEffect(() => {
 		highlightCode()
 			.then((res) => { setIsCodeHighlighted(res); })
-			.catch(() => { setIsCodeHighlighted(false); })
-	}, [])
+			.catch(() => { setIsCodeHighlighted(false); });
+	}, []);
 
-	return <main>
-		<StickyWrapper>
-			<div className='flex align-items-center justify-content-between' >
-				<h1>NPM Package Tutorial</h1>
+	return (
+		<main>
+			<StickyWrapper>
+				<div className='flex align-items-center justify-content-between'>
+					<h1>NPM Package Tutorial</h1>
 
-				
-				<div className='flex align-items-center' >
-				Code Theme: &nbsp;
-				<Select
-					options={THEME_OPTIONS}
-					value={codeStyleTheme}
-					onChange={setCodeStyleTheme}
-				/>
+					<div className='flex align-items-center'>
+						Code Theme: &nbsp;
+						<Select options={THEME_OPTIONS} value={codeStyleTheme} onChange={setCodeStyleTheme} />
+					</div>
 				</div>
-			</div>
-		</StickyWrapper>
+			</StickyWrapper>
 
-		{tutorialSteps.map(
-			(step: tutorialStep) => (
-				<InstructionWrapper isCodeHighlighted={isCodeHighlighted} {...step} />
-			)
-		)}
-		<link rel='stylesheet' href={`/codeHighlighterThemes/${codeStyleTheme}.css`}></link>
-	</main>
+			<p>
+				In this tutorial we will learn to create a simple basic react library, which will provide simple basic
+				reusable components.
+			</p>
+
+			{isCodeHighlighted == null ? (
+				<Loader variant="spin" color="#ff5733" size="40px" />
+			) : (
+				tutorialSteps.map((step: tutorialStep) => (
+					<InstructionWrapper isCodeHighlighted={isCodeHighlighted} {...step} />
+				))
+			)}
+
+			<link rel='stylesheet' href={`/codeHighlighterThemes/${codeStyleTheme}.css`}></link>
+		</main>
+	);
 }
