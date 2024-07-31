9aadeebf6490bc5abf4611ee0c7b4098756d6d55
Dinkar <aakashdinkarh@gmail.com>
2024-08-01 00:09:54 +0530

    mixpanel object to class, encapsulation, controlled access, singleton pattern

--- src/app/utils/mixpanel.ts
+++ src/app/utils/mixpanel.ts
@@ -2,32 +2,35 @@ import type { OverridedMixpanel } from 'mixpanel-browser';
 
 import { DEVELOPMENT, PRODUCTION } from '../constants';
 
-const getMixpanel = async (): Promise<OverridedMixpanel> => {
-	const module = (await import('mixpanel-browser')).default;
-	return module;
-};
+class MixpanelService {
+	private mixpanel: OverridedMixpanel | null = null;
+	private queue: (() => void)[] = [];
 
-const getMixpanelToken = () => {
-	switch (process.env.REACT_APP_ENVIRONMENT) {
-		case DEVELOPMENT:
-			return process.env.REACT_APP_MIXPANEL_TOKEN_DEV;
-		case PRODUCTION:
-			return process.env.REACT_APP_MIXPANEL_TOKEN_PROD;
-		default:
-			return null;
+	private static isDevelopment = process.env.REACT_APP_ENVIRONMENT === DEVELOPMENT;
+	private static isProduction = process.env.REACT_APP_ENVIRONMENT === PRODUCTION;
+
+	private static getMixpanelToken(): string | null {
+		switch (process.env.REACT_APP_ENVIRONMENT) {
+			case DEVELOPMENT:
+				return process.env.REACT_APP_MIXPANEL_TOKEN_DEV ?? null;
+			case PRODUCTION:
+				return process.env.REACT_APP_MIXPANEL_TOKEN_PROD ?? null;
+			default:
+				return null;
+		}
 	}
-};
 
-export const mixpanel = {
-	mixpanel : null as OverridedMixpanel | null,
-	queue    : [] as (() => void)[],
+	private static async getMixpanel(): Promise<OverridedMixpanel> {
+		const module = (await import('mixpanel-browser')).default;
+		return module;
+	}
 
-	async init() {
+	public async init(): Promise<void> {
 		try {
-			this.mixpanel = await getMixpanel();
-			const mixpanelToken = getMixpanelToken();
+			this.mixpanel = await MixpanelService.getMixpanel();
+			const mixpanelToken = MixpanelService.getMixpanelToken();
 			if (mixpanelToken != null && mixpanelToken !== '') {
-				this.mixpanel.init(mixpanelToken);
+				this.mixpanel.init(mixpanelToken, { debug: MixpanelService.isDevelopment, ip: true });
 			}
 			this.queue.forEach((fn: () => void) => { fn(); });
 			this.queue = [];
@@ -35,16 +38,21 @@ export const mixpanel = {
 			// eslint-disable-next-line no-console
 			console.error(err);
 		}
-	},
-	track(eventName: string, properties?: Record<string, unknown>) {
+	}
+
+	public track(eventName: string, properties?: Record<string, unknown>): void {
 		if (this.mixpanel) {
-			// todo: remove console.log	
-			console.log('Mixpanel track', eventName, properties);
-			// this.mixpanel.track(eventName, properties);
+			if (MixpanelService.isProduction) {
+				this.mixpanel.track(eventName, properties);
+				return;
+			}
+			console.log(eventName, properties);
 		} else {
 			this.queue.push(() => {
 				this.track(eventName, properties);
 			});
 		}
-	},
-} as const;
+	}
+}
+
+export const mixpanel = new MixpanelService();
